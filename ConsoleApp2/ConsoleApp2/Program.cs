39789d67dd38ba65011707f702c243ff66c9ad35
lalit <LALIT.KATHURIA@niit-tech.com>
2019-12-04 22:38:03 +0530

    ddd

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json.Linq;
 
 namespace ConsoleApp2
 {
-   public  class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -15,41 +15,7 @@ namespace ConsoleApp2
         }
         public static void Decode(string token)
         {
-            string[] parts = token.Split('.');
-            string headerJson = parts[0];
-            string payloadJson = parts[1];
-            var crypto = parts[2];
-
-
-           // JObject headerData = JObject.Parse(headerJson);
-
-
-            //JObject payloadData = JObject.Parse(payloadJson);
-
-
-            byte[] key = Encoding.UTF8.GetBytes("aaaaaaaaaaaaaaaaa");
-            byte[] message = Encoding.UTF8.GetBytes(payloadJson);
-
-            string sig =Convert.ToBase64String( HashHMAC(key, message))
-                .Replace('+', '-')
-                .Replace('/', '_')
-                .Replace("=", "");
-
-            Console.WriteLine(Encoding.UTF8.GetString(message));
-            Console.WriteLine(sig);
-
-            Console.WriteLine(Convert.ToBase64String(Encoding.UTF8.GetBytes(crypto)));
-            Console.Read();
-
-
-            //  return payloadData.ToString();
+            Console.WriteLine(token);
         }
-
-        private static byte[] HashHMAC(byte[] key, byte[] message)
-        {
-            var hash = new HMACSHA256(key);
-            return hash.ComputeHash(message);
-        }
-
     }
 }
